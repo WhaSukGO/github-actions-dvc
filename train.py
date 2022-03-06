e8ee2eba60a3322257f6bb2ec75eb7a3402558e6
WhaSukGO <leeju213@gmail.com>
2022-03-06 21:50:56 +0900

    Logistic -> QuadraticDiscriminant

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
-import pandas as pd 
+import pandas as pd
 import numpy as np
-from sklearn.linear_model import LogisticRegression
+# from sklearn.linear_model import LogisticRegression
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn import preprocessing
 from sklearn.model_selection import cross_val_predict
 from sklearn.metrics import confusion_matrix
@@ -13,13 +14,13 @@ from sklearn.impute import SimpleImputer
 
 df = pd.read_csv("data_processed.csv")
 
-#### Get features ready to model! 
+# Get features ready to model!
 y = df.pop("cons_general").to_numpy()
-y[y< 4] = 0
-y[y>= 4] = 1
+y[y < 4] = 0
+y[y >= 4] = 1
 
 X = df.to_numpy()
-X = preprocessing.scale(X) # Is standard
+X = preprocessing.scale(X)  # Is standard
 # Impute NaNs
 
 imp = SimpleImputer(missing_values=np.nan, strategy='mean')
@@ -28,17 +29,19 @@ X = imp.transform(X)
 
 
 # Linear model
-clf = LogisticRegression()
+# clf = LogisticRegression()
+clf = QuadraticDiscriminantAnalysis()
 yhat = cross_val_predict(clf, X, y, cv=5)
 
-acc = np.mean(yhat==y)
+acc = np.mean(yhat == y)
 tn, fp, fn, tp = confusion_matrix(y, yhat).ravel()
 specificity = tn / (tn+fp)
 sensitivity = tp / (tp + fn)
 
 # Now print to file
 with open("metrics.json", 'w') as outfile:
-        json.dump({ "accuracy": acc, "specificity": specificity, "sensitivity":sensitivity}, outfile)
+    json.dump({"accuracy": acc, "specificity": specificity,
+               "sensitivity": sensitivity}, outfile)
 
 # Let's visualize within several slices of the dataset
 score = yhat == y
@@ -48,6 +51,6 @@ df['pred_accuracy'] = score_int
 # Bar plot by region
 
 sns.set_color_codes("dark")
-ax = sns.barplot(x="region", y="pred_accuracy", data=df, palette = "Greens_d")
-ax.set(xlabel="Region", ylabel = "Model accuracy")
-plt.savefig("by_region.png",dpi=80)
+ax = sns.barplot(x="region", y="pred_accuracy", data=df, palette="Greens_d")
+ax.set(xlabel="Region", ylabel="Model accuracy")
+plt.savefig("by_region.png", dpi=80)
